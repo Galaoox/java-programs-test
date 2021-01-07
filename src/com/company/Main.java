c5966834bae6d8e6018fde9f99720cab8cec8e9a
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2021-01-06 21:16:32 -0500

    exercise 1.3.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String name = scanner.nextLine();
+        System.out.println("Hola Mundo " + name);
+    }
+}
