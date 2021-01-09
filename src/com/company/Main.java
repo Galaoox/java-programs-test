e932382bbf18bf9e9700c42c337357405f059df3
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2021-01-09 17:24:45 -0500

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un valor: ");
+        System.out.println("Ingrese  un dia de la semana (numero): ");
         int num = scanner.nextInt();
-        int resto = num % 2;
-        String msg = resto == 0 ? "El valor ingresado es par" : "El valor ingresado es impar";
-        System.out.println(num + " " + msg);
+        String day;
+        switch(num){
+            case 1:
+                day = "Lunes";
+                break;
+            case 2:
+                day = "Martes";
+                break;
+            case 3:
+                day = "Miercoles";
+                break;
+            case 4:
+                day = "Jueves";
+                break;
+            case 5:
+                day = "Viernes";
+                break;
+            case 6:
+                day = "Sabado";
+                break;
+            case 7:
+                day = "Domingo";
+                break;
+            default:
+                day = "Dia incorrecto... el valor debe ser entre el 1 y el 7.";
+        }
+        System.out.println(day);
 
 
     }
