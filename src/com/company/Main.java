38b399726d3cbd5fdfba825c836b5d7a96748a91
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2021-01-09 17:51:41 -0500

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,48 +4,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Defino las contantes
-        // Las constantes van con publi static y final pero en este caso solo usare el final
-         final int LUNES = 1;
-         final int MARTES = 2;
-         final int MIERCOLES = 3;
-         final int JUEVES = 4;
-         final int VIERNES = 5;
-         final int SABADO = 6;
-         final int DOMINGO = 7;
-
-
+        int arr[] = new int[10];
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese  un dia de la semana (numero): ");
-        int num = scanner.nextInt();
-        String day;
-        switch(num){
-            case LUNES:
-                day = "Lunes";
-                break;
-            case MARTES:
-                day = "Martes";
-                break;
-            case MIERCOLES:
-                day = "Miercoles";
-                break;
-            case JUEVES:
-                day = "Jueves";
-                break;
-            case VIERNES:
-                day = "Viernes";
-                break;
-            case SABADO:
-                day = "Sabado";
-                break;
-            case DOMINGO:
-                day = "Domingo";
-                break;
-            default:
-                day = "Dia incorrecto... el valor debe ser entre el 1 y el 7.";
-        }
-        System.out.println(day);
+        System.out.println("Ingrese un valor (0 == fin): ");
+        int v = scanner.nextInt();
+        int i = 0;
 
+        while(v != 0 && i < 10){
+            arr[i++] = v;
+            System.out.println("Ingrese un valor (0 == fin): ");
+            v = scanner.nextInt();
+        }
+
+        for(int j = 0 ; j < i ; j++){
+            System.out.println(arr[j]);
+        }
 
     }
 }
